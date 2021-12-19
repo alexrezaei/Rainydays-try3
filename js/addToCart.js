d8dacc7a32b28ea7bb100e6fd537bd6c09bc017d
alexrezaei <74854699+alexrezaei@users.noreply.github.com>
2021-12-19 21:11:22 +0100

    update to cart functionality

--- js/addToCart.js
+++ js/addToCart.js
@@ -8,6 +8,10 @@ colors.addEventListener("change", (e) => {
 // size
 let size = "small";
 const sizes = document.querySelector("#sizes");
+
+sizes.addEventListener("change", (e) => {
+  size = sizes.value;
+});
 // shopping cart
 const shoppingCart = document.querySelector(".shopping-cart");
 const addToCart = document.querySelector(".add-cart");
@@ -18,5 +22,12 @@ addToCart.addEventListener("click", () => {
   <img src="/img/raincoat.jpeg" alt="raincoat">
   <p>Color:${color}</p>
   <p>Size:${size}</p>
+  <button id="remove-cart">Remove from cart</button>
   `;
+});
+
+shoppingCart.addEventListener("click", (e) => {
+  if (e.target.id == "remove-cart") {
+    shoppingCart.innerHTML = "<p>Empty<p>";
+  }
 });
